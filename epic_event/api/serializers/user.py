e7a45a75f5191745373b95e81dba346d83769e42
ribandgel <ribeiroandgel@gmx.fr>
2022-05-05 19:28:47 +0200

    Filter

--- epic_event/api/serializers/user.py
+++ epic_event/api/serializers/user.py
@@ -10,6 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = (
             "id",
+            "username",
             "email",
             "first_name",
             "last_name",
