7ae9c74f6c741bb7205669436ee7e8177854164a
Andgel Ribeiro <andgelribeiro@gmail.com>
2022-09-15 11:31:17 +0200

    Automatic event creation after the contract is signed Event can't be created if the contract is not signed
    Lint

--- epic_event/api/admin.py
+++ epic_event/api/admin.py
@@ -1,20 +1,26 @@
 from django.contrib import admin
-from epic_event.api.models import User, Contract, Event
+
+from epic_event.api.models import Contract, Event, User
 
 
 class AdminSite(admin.AdminSite):
     site_header = "Admin Epic Event"
 
+
 admin_site = AdminSite(name="Admin")
+
 
 class UserAdmin(admin.ModelAdmin):
     pass
 
+
 class ContractAdmin(admin.ModelAdmin):
-   pass
+    pass
+
 
 class EventAdmin(admin.ModelAdmin):
-   pass
+    pass
+
 
 admin_site.register(User, UserAdmin)
 admin_site.register(Contract, ContractAdmin)
